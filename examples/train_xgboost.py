0ac99c52ad009eaba6f2270f7a7e92074b39914e
Ignat Drozdov <idrozdov@beringresearch.com>
2018-08-27 14:44:40 +0100

    add xgboost as an example

---
+++ examples/train_xgboost.py
@@ -0,0 +1,29 @@
+from xgboost import XGBClassifier
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score
+
+from lab.sklearn import Experiment
+
+if __name__ == "__main__":
+    e = Experiment()
+
+    @e.start_run
+    def train():        
+        iris = datasets.load_iris()
+        X = iris.data
+        y = iris.target
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.24, random_state=42)
+
+        clf = XGBClassifier()
+        clf.fit(X_train, y_train)
+
+        y_pred = clf.predict(X_test)
+        accuracy = accuracy_score(y_test, y_pred)
+        precision = precision_score(y_test, y_pred, average = 'macro')
+
+        e.log_metric('accuracy_score', accuracy)
+        e.log_metric('precision_score', precision)        
+
+        e.log_model(clf, 'xgboost')
