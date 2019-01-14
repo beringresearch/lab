9b9ee57df0077e49a181b535703a7467a4fc9fd5
Ignat Drozdov <idrozdov@beringresearch.com>
2019-01-14 19:35:00 +0000

    create venv if it isn't found upon file execution through lab run

--- lab/project.py
+++ lab/project.py
@@ -8,27 +8,17 @@ import shutil
 
 import venv as ve
 
-def project_init(name, r):
-    # Create project structure
-    os.mkdir(name)
-    os.mkdir(os.path.join(name, 'notebooks'))
-    os.mkdir(os.path.join(name, 'data'))
-    os.mkdir(os.path.join(name, 'logs'))
-    os.mkdir(os.path.join(name, 'models'))
-    os.mkdir(os.path.join(name, 'config'))
+from shutil import copyfile
 
-    open(os.path.join(name, 'README.md'), 'a').close()
-
+def create_venv(project_name):
     # Create a virtual environment
-    venv_dir = os.path.join(name, '.venv')
+    venv_dir = os.path.join(project_name, '.venv')
 
     environment = ve.EnvBuilder(symlinks=True, with_pip=True)
     environment.create(venv_dir)
     
-    if r is not None:
-        #subprocess.call([venv_dir + '/bin/pip', 'install', 'virtualenv'])
-        subprocess.call([venv_dir + '/bin/pip', 'install', 'pyyaml'])
-        subprocess.call([venv_dir + '/bin/pip', 'install', '-r', r])
+    subprocess.call([venv_dir + '/bin/pip', 'install', 'pyyaml'])
+    subprocess.call([venv_dir + '/bin/pip', 'install', '-r', 'requirements.txt'])
     
     # Move lab into the virtual environment
     pkgname = 'lab'    
@@ -43,13 +33,46 @@ def project_init(name, r):
     pkgdir = os.path.dirname(pkgobj.__file__)
     dst = '%s/lib/python%s/site-packages/%s' % (venv_dir, pyversion, pkgname)
     shutil.copytree(pkgdir, dst)
+
+def project_init(project_name):
+    pyversion = '%s.%s' % (sys.version_info[0], sys.version_info[1])
+    
+    # Create project structure
+    os.mkdir(project_name)
+    os.mkdir(os.path.join(project_name, 'notebooks'))
+    os.mkdir(os.path.join(project_name, 'data'))
+    os.mkdir(os.path.join(project_name, 'logs'))
+    os.mkdir(os.path.join(project_name, 'models'))
+    os.mkdir(os.path.join(project_name, 'config'))
+
+    open(os.path.join(project_name, 'README.md'), 'a').close()
+
+    # Copy requirements.txt file
+    copyfile('requirements.txt', project_name+'/requirements.txt')
+
+    # Create a virtual environment
+    create_venv(project_name)
     
 
     # Create runtime configuration
-    runtime = {'name': name,
+    runtime = {'name': project_name,
                'description': None,
-               'python': pyversion
+               'python': pyversion,
                'timestamp': str(datetime.datetime.now()),
                'venv': '.venv'}
-    with open(os.path.join(name, 'config', 'runtime.yaml'), 'w') as file:
+    with open(os.path.join(project_name, 'config', 'runtime.yaml'), 'w') as file:
         yaml.dump(runtime, file, default_flow_style=False)
+
+    # Generate a Dockerfile
+    #f.write('FROM python:%s\n' % (pyversion))
+    #f = open(os.path.join(name, 'Dockerfile'), 'w')
+    #f.write('COPY .venv .\n')
+    #f.write('COPY config .\n')
+    #f.write('COPY data .\n')
+    #f.write('COPY logs .\n')
+    #f.write('COPY models .\n')
+    #f.write('COPY notebooks .\n')
+    #f.write('COPY requirements.txt .\n')
+    #f.write('COPY README.md .')    
+    #f.close()
+    
