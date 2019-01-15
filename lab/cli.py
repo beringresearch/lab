ec0ba84f71f5c23d5f1d0aa345df56339581f59e
Ignat Drozdov <idrozdov@beringresearch.com>
2019-01-15 05:44:44 +0000

    add stable error handling

--- lab/cli.py
+++ lab/cli.py
@@ -47,57 +47,63 @@ def lab_run(script):
 def compare_experiments(sort_by = None):
     models_directory = 'models'
     logs_directory = 'logs'
-    
-    if os.path.isdir(models_directory):
-        experiments = next(os.walk(models_directory))[1]
-    else:
-        raise Exception("lab ls must be run from a directory that contains models folder.")
-    
     TICK = '█'
 
-    comparisons = []    
+    if not os.path.exists(models_directory):
+        click.echo('This directory does not appear to contain a valid lab project. Run lab init to create one.')
+        click.Context.abort(cli)
+    
+    experiments = next(os.walk(models_directory))[1]
+    if len(experiments) == 0:
+        click.echo("It looks like you've started a brand new project. Run your first experiment to generate a list of metrics.")
+        click.Context.abort(cli)
+    comparisons = []
+
     for e in experiments:
         metrics_file = os.path.join(models_directory, e, 'metrics.yaml')
-        with open(metrics_file, 'r') as file:
-            metrics = yaml.load(file)
-        for k, v in metrics.items():
-            metrics[k] = round(v, 2)        
+        try:
+            with open(metrics_file, 'r') as file:
+                metrics = yaml.load(file)
+            for k, v in metrics.items():
+                metrics[k] = round(v, 2)        
         
-        metrics_list = list(metrics.values())
+            metrics_list = list(metrics.values())
         
-        meta_file = os.path.join(logs_directory, e, 'meta.yaml')
-        with open(meta_file, 'r') as file:
-            meta = yaml.load(file)
+            meta_file = os.path.join(logs_directory, e, 'meta.yaml')
+            with open(meta_file, 'r') as file:
+                meta = yaml.load(file)
                             
-        record = [meta['experiment_uuid'], meta['source'], str(meta['start_time'].date())] + metrics_list
-        comparisons.append(record)
+            record = [meta['experiment_uuid'], meta['source'], str(meta['start_time'].date())] + metrics_list
+            comparisons.append(record)
 
-    # Create visualisation of numeric metrics
-    A = pd.DataFrame(comparisons)
-    meta_data = A[[0, 1, 2]]
-    metrics_data = A.drop([0, 1, 2], axis = 1)
+            # Create visualisation of numeric metrics
+            A = pd.DataFrame(comparisons)
+            meta_data = A[[0, 1, 2]]
+            metrics_data = A.drop([0, 1, 2], axis = 1)
     
-    row_max = metrics_data.abs().max(axis = 0)    
-    scaled_metrics_data = metrics_data.abs().divide(row_max, axis = 1)
-    scaled_metrics_data = scaled_metrics_data.fillna(value = 0)
+            row_max = metrics_data.abs().max(axis = 0)    
+            scaled_metrics_data = metrics_data.abs().divide(row_max, axis = 1)
+            scaled_metrics_data = scaled_metrics_data.fillna(value = 0)
     
-    sparklines = np.empty(shape = metrics_data.shape, dtype=object)
-    for row in range(metrics_data.shape[0]):
-        for column in range(metrics_data.shape[1]):
-            value = metrics_data.iloc[row, column]
-            scaled_value = scaled_metrics_data.iloc[row, column]
-            scaled_value = scaled_value
-            spark = format(value, '.2f')+': '+TICK * int(round(scaled_value*10))
-            sparklines[row, column] = spark
+            sparklines = np.empty(shape = metrics_data.shape, dtype=object)
+            for row in range(metrics_data.shape[0]):
+                for column in range(metrics_data.shape[1]):
+                    value = metrics_data.iloc[row, column]
+                    scaled_value = scaled_metrics_data.iloc[row, column]
+                    scaled_value = scaled_value
+                    spark = format(value, '.2f')+': '+TICK * int(round(scaled_value*10))
+                    sparklines[row, column] = spark
 
-    result = pd.concat([meta_data, pd.DataFrame(sparklines)], axis = 1)
-    result.columns = ['Experiment', 'Source', 'Date'] + list(metrics.keys())    
+            result = pd.concat([meta_data, pd.DataFrame(sparklines)], axis = 1)
+            result.columns = ['Experiment', 'Source', 'Date'] + list(metrics.keys())    
 
-    if sort_by is not None:
-        result.sort_values(by = [sort_by], axis = 0, ascending=False, inplace = True)
+            if sort_by is not None:
+                result.sort_values(by = [sort_by], axis = 0, ascending=False, inplace = True)
 
-    header = ['Experiment', 'Source', 'Date'] + list(metrics.keys())
-    click.echo(tabulate.tabulate(result.values, headers = header))
+            header = ['Experiment', 'Source', 'Date'] + list(metrics.keys())
+            click.echo(tabulate.tabulate(result.values, headers = header))
+        except:
+            pass
 
 cli.add_command(lab_init)
 cli.add_command(lab_run)
