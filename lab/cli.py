a34d12221153d8f12f7153efca4e2d751f858e8a
Ignat Drozdov <idrozdov@beringresearch.com>
2019-01-25 10:31:28 +0000

    check for outdated lab package, add multiple minio hosts

--- lab/cli.py
+++ lab/cli.py
@@ -13,7 +13,6 @@ import warnings
 import pandas as pd
 import numpy as np
 
-import lab
 from lab.project import project_init, create_venv, push_to_minio
 
 
@@ -51,24 +50,35 @@ def lab_run(script):
     try:
         with open(os.path.join(os.getcwd(), 'config', 'runtime.yaml'), 'r') as file:
             config = yaml.load(file)
-            home_dir = config['path']
-    except:
+            home_dir = config['path']            
+    except FileNotFoundError:
         click.echo("Having trouble reading configuration file for this project. \nIt's most likely that this is either not a Lab Project or the Project was created with an older version of Lab.\n")
         click.Context.abort(cli)
+    else:
+        # Update project directory if it hasn't been updated        
+        if home_dir != os.getcwd():
+            config['path'] = os.getcwd()
+            with open(os.path.join(os.getcwd(), 'config', 'runtime.yaml'), 'w') as file:
+                yaml.dump(config, file, default_flow_style=False)
 
     if not os.path.exists(os.path.join(home_dir, '.venv')):
-        click.echo('virtual environment not found. Creating one for this project')
+        click.echo('Virtual environment not found. Creating one for this project')
         create_venv(home_dir)
-
-    # Check that venv and global Lab versions match
+    
+    
+    # Extract lab version from virtual environment
     pyversion = '%s.%s' % (sys.version_info[0], sys.version_info[1])
     venv = '%s/lib/python%s/site-packages/%s' % ('.venv', pyversion, 'lab')
-    spec = importlib.util.spec_from_file_location('lab', os.path.join(venv, '__init__.py'))
-    foo = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(foo)
-    venv_lab_version = foo.__version__
+    
+    python_bin = os.path.join(home_dir, '.venv', 'bin/python')
+    call = 'import lab; print(lab.__version__)'
+    venv_lab_version = subprocess.check_output([python_bin, '-c', '%s' % call])
+    venv_lab_version = venv_lab_version.decode('ascii').rstrip()
+
+    import lab
+    global_lab_version = lab.__version__    
         
-    if lab.__version__ != venv_lab_version:
+    if  global_lab_version != venv_lab_version:
         click.echo('It appears that your Lab Project was built using a different Lab version (' + venv_lab_version + ').')
         if click.confirm('Do you want to sync?'):
             try:
@@ -154,9 +164,10 @@ def compare_experiments(sort_by = None):
         pass
 
 @click.command('push')
+@click.option('--tag', type=str, help='minio host nickname')
 @click.option('--bucket', type=str, default=str(uuid.uuid4()), help='minio bucket name')
 @click.argument('path', type=str, default=os.getcwd())
-def lab_push(bucket, path):
+def lab_push(tag, bucket, path):
     """ Push lab experiment to minio """
     models_directory = 'experiments'
     logs_directory = 'logs'
@@ -171,18 +182,21 @@ def lab_push(bucket, path):
         click.echo('This directory does not appear to contain a valid lab project. Run lab init to create one.')
         click.Context.abort(cli)
     
-    push_to_minio(bucket, path)
+    push_to_minio(tag, bucket, path)
 
     
 
 @click.command('config')
+@click.option('--tag', type=str, help='helpful minio host tag')
 @click.option('--endpoint', type=str, help='minio endpoint address')
 @click.option('--accesskey', type=str, help='minio access key')
 @click.option('--secretkey', type=str, help='minio secret key')
-def lab_config(endpoint, accesskey, secretkey):
+def lab_config(tag, endpoint, accesskey, secretkey):
     """ Configure the lab environment and setup remote file storage"""
     home_dir = os.path.expanduser('~')
     lab_dir = os.path.join(home_dir, '.lab')
+
+    # Test connection
     if not os.path.exists(lab_dir):
         os.makedirs(lab_dir)
 
@@ -195,13 +209,25 @@ def lab_config(endpoint, accesskey, secretkey):
         click.echo('Cannot connect to minio instance. Check your credentials and hostname. Ensure that endpoint is not prefixed with http or https.')
         click.Context.abort(cli)
 
-    
+    # Create configuration
     config = {'minio_endpoint': endpoint,
               'minio_accesskey': accesskey,
-              'minio_secretkey': secretkey}
+              'minio_secretkey': secretkey}    
 
+    if os.path.exists(os.path.join(lab_dir, 'config.yaml')):
+        with open(os.path.join(lab_dir, 'config.yaml'), 'r') as file:
+            minio_config = yaml.safe_load(file)
+            if tag in minio_config.keys():
+                click.echo('Host tag '+tag+' already exists in your configuration. Try a different name.')
+                click.Context.abort(cli)
+
+            minio_config[tag] = config
+    else:
+        minio_config = {}
+        minio_config[tag] = config
+    
     with open(os.path.join(lab_dir, 'config.yaml'), 'w') as file:
-        yaml.dump(config, file, default_flow_style=False)
+        yaml.safe_dump(minio_config, file, default_flow_style=False)
 
 cli.add_command(lab_init)
 cli.add_command(lab_run)
